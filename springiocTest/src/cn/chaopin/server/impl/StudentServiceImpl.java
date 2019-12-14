c26dae450975ade62f39c07c63df217369a9a678
aleam0722 <cxb444555@outlook.com>
2019-12-14 15:41:31 +0800

    创建实现了StudentService接口的实现类StudentServiceImpl

---
+++ springiocTest/src/cn/chaopin/server/impl/StudentServiceImpl.java
@@ -0,0 +1,12 @@
+package cn.chaopin.server.impl;
+
+import cn.chaopin.pojo.Student;
+import cn.chaopin.server.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+
+    @Override
+    public Student get(String id) {
+        return null;
+    }
+}
