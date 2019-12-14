0551c48a27e7b63cf0f516ab70501144385d8452
aleam0722 <cxb444555@outlook.com>
2019-12-14 15:52:54 +0800

    在web层中获取到studentService对象

--- springiocTest/src/cn/chaopin/web/OutTest.java
+++ springiocTest/src/cn/chaopin/web/OutTest.java
@@ -2,6 +2,8 @@ package cn.chaopin.web;
 
 import cn.chaopin.server.StudentService;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class OutTest {
 
@@ -9,6 +11,9 @@ public class OutTest {
 
     @Test
     public void Test(){
+//        在web层获取到studentService对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        StudentService studentService = (StudentService) applicationContext.getBean("studentService");
     }
 }
 
