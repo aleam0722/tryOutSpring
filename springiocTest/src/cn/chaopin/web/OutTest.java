92138ac6efbf1f85d4856d6a854c2fa4aa965129
aleam0722 <cxb444555@outlook.com>
2019-12-14 15:33:00 +0800

    测试Spring的注解开发方式，创建mudle并且建立文件目录，在pojo目录中创建了Student的bean类

---
+++ springiocTest/src/cn/chaopin/web/OutTest.java
@@ -0,0 +1,9 @@
+package cn.chaopin.web;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class OutTest {
+
+}
+
